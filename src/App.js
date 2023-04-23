34f73e83b0153e5cb45634d67f53e638bf262d66
Rick <ricksmycodeguy@gmail.com>
2023-04-23 13:14:02 -0500

    Add option to add aditional movies

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
+import AddMovie from './components/AddMovie';
 import "./App.css";
 
 function App() {
@@ -40,6 +41,12 @@ function App() {
     fetchMoviesHander();
   }, [fetchMoviesHander]);
 
+  function addMovieHandler(movie) {
+    setMovies((prevMovies) => {
+      return [movie, ...prevMovies];
+    });
+  }
+
   let content = <p>Found no movies.</p>;
   if (movies.length > 0) {
     content = <MoviesList movies={movies} />;
@@ -59,6 +66,9 @@ function App() {
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={ addMovieHandler } />
+        </section>
       <section>
         <button onClick={fetchMoviesHander}>Fetch Movies</button>
         <div className="date">Current Date {date}</div>
